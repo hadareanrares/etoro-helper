6456bfdb562bb5e28d1dcdecd1f49bca7ee28f33
Rares Hadarean <hadareanrares@gmail.com>
2017-12-23 11:06:38 +0200

    fix Xcode indentation

--- Resources/script.js
+++ Resources/script.js
@@ -6,42 +6,42 @@ function initWatchListView() {
         if (!mainContentEl) {
             return;
         }
-        
+
         prepareView(mainContentEl);
-        
+
         cleanSentiment();
-        
+
         let isListView = mainContentEl.className.indexOf('list-view') > 0;
         if (isListView) {
             let marketEl = mainContentEl.querySelector('.table-body.market');
             let tabletRowEls = marketEl.querySelectorAll('.table-body .table-row');
             tabletRowEls.forEach((el) => {
-                                 if (el.className.indexOf('empty') !== -1) {
-                                 return;
-                                 }
-                                 let tableInfoEl = el.querySelector('.table-info');
-                                 let sellBtnEl = tableInfoEl.querySelector('.etoro-sell-button');
-                                 let buyBtnEl = tableInfoEl.querySelector('.etoro-buy-button');
-                                 if (!sellBtnEl || !buyBtnEl) {
-                                 return;
-                                 }
-                                 let sellPrice = sellBtnEl.querySelector('.etoro-price-value').textContent.trim();
-                                 let buyPrice = buyBtnEl.querySelector('.etoro-price-value').textContent.trim();
-                                 createSpreadColumn(tableInfoEl, buyPrice, sellPrice);
-                                 });
+                if (el.className.indexOf('empty') !== -1) {
+                    return;
+                }
+                let tableInfoEl = el.querySelector('.table-info');
+                let sellBtnEl = tableInfoEl.querySelector('.etoro-sell-button');
+                let buyBtnEl = tableInfoEl.querySelector('.etoro-buy-button');
+                if (!sellBtnEl || !buyBtnEl) {
+                    return;
+                }
+                let sellPrice = sellBtnEl.querySelector('.etoro-price-value').textContent.trim();
+                let buyPrice = buyBtnEl.querySelector('.etoro-price-value').textContent.trim();
+                createSpreadColumn(tableInfoEl, buyPrice, sellPrice);
+            });
         } else {
             let cardListEls = mainContentEl.querySelectorAll('.market-card-ph.pointer');
             cardListEls.forEach((cardEl) => {
-                                let btnSellEl = cardEl.querySelector('.etoro-sell-button');
-                                let btnBuyEl = cardEl.querySelector('.etoro-buy-button');
-                                if (!btnBuyEl || !btnSellEl) {
-                                return;
-                                }
-                                let sellPrice = btnSellEl.querySelector('.etoro-price-value').textContent.trim();
-                                let buyPrice = btnBuyEl.querySelector('.etoro-price-value').textContent.trim();
-                                let marketHeadEl = cardEl.querySelector('.market-card-head');
-                                createSpreadColumn(marketHeadEl, buyPrice, sellPrice);
-                                });
+                let btnSellEl = cardEl.querySelector('.etoro-sell-button');
+                let btnBuyEl = cardEl.querySelector('.etoro-buy-button');
+                if (!btnBuyEl || !btnSellEl) {
+                    return;
+                }
+                let sellPrice = btnSellEl.querySelector('.etoro-price-value').textContent.trim();
+                let buyPrice = btnBuyEl.querySelector('.etoro-price-value').textContent.trim();
+                let marketHeadEl = cardEl.querySelector('.market-card-head');
+                createSpreadColumn(marketHeadEl, buyPrice, sellPrice);
+            });
         }
     } catch (e) {
         console.log(e);
@@ -53,19 +53,19 @@ function initialPortfolioOverview() {
         let portfolioEl = document.querySelector('.main-app-view .p-portfolio .portfolio-overview');
         let tabletRowEls = portfolioEl.querySelectorAll('.ui-table-row-container');
         tabletRowEls.forEach((el) => {
-                             if (el.className.indexOf('empty') !== -1) {
-                             return;
-                             }
-                             let cellNameEl = el.querySelector('.ui-table-static-cell');
-                             let sellBtnEl = el.querySelector('.etoro-sell-button');
-                             let buyBtnEl = el.querySelector('.etoro-buy-button');
-                             if (!sellBtnEl || !buyBtnEl) {
-                             return;
-                             }
-                             let sellPrice = sellBtnEl.querySelector('.etoro-price-value').textContent.trim();
-                             let buyPrice = buyBtnEl.querySelector('.etoro-price-value').textContent.trim();
-                             createSpreadColumn(cellNameEl, buyPrice, sellPrice);
-                             });
+            if (el.className.indexOf('empty') !== -1) {
+                return;
+            }
+            let cellNameEl = el.querySelector('.ui-table-static-cell');
+            let sellBtnEl = el.querySelector('.etoro-sell-button');
+            let buyBtnEl = el.querySelector('.etoro-buy-button');
+            if (!sellBtnEl || !buyBtnEl) {
+                return;
+            }
+            let sellPrice = sellBtnEl.querySelector('.etoro-price-value').textContent.trim();
+            let buyPrice = buyBtnEl.querySelector('.etoro-price-value').textContent.trim();
+            createSpreadColumn(cellNameEl, buyPrice, sellPrice);
+        });
     } catch (e) {
         console.log(e);
     }
@@ -74,35 +74,35 @@ function initialPortfolioOverview() {
 function createSpreadColumn(parent, buyPrice, sellPrice) {
     let clsHelperPrice = 'etoro-helper-price';
     let priceDiv = parent.querySelector('.' + clsHelperPrice);
-    
+
     let spreadPrice = Number(buyPrice) - Number(sellPrice);
     let spreadPercent = (spreadPrice / buyPrice) * 100;
-    
+
     let amount = toFixed(spreadPrice);
     let percentage = spreadPercent.toFixed(3) + '%';
-    
+
     if (!priceDiv) {
         let priceDiv = document.createElement('div');
         priceDiv.className += "table-cell small-cell " + clsHelperPrice;
-        
+
         let amountSpan = document.createElement('span');
         amountSpan.className += "gain-num-amount";
         let amountNode = document.createTextNode(amount);
         amountSpan.appendChild(amountNode);
-        
+
         let percentageSpan = document.createElement('span');
         percentageSpan.className += "gain-num-protsent";
         let percentageNode = document.createTextNode(percentage);
         percentageSpan.appendChild(percentageNode);
-        
+
         priceDiv.appendChild(amountSpan);
         priceDiv.appendChild(percentageSpan);
-        
+
         parent.insertBefore(priceDiv, parent.children[2]);
     } else {
         let amountSpan = priceDiv.querySelector('.gain-num-amount');
         amountSpan.innerHTML = amount;
-        
+
         let percentageSpan = priceDiv.querySelector('.gain-num-protsent');
         percentageSpan.innerHTML = percentage;
     }
@@ -134,8 +134,8 @@ function updatePrice() {
 function cleanSentiment() {
     let sentimentElements = document.querySelectorAll('.main-app-view .sentiment');
     sentimentElements.forEach((element) => {
-                              element.classList.remove('sentiment');
-                              });
+        element.classList.remove('sentiment');
+    });
 }
 
 function prepareView(contentElement) {
@@ -145,9 +145,9 @@ function prepareView(contentElement) {
     }
     console.log('preparing view');
     let sentimentColumn = headerElement.children[2];
-    
+
     headerElement.removeChild(headerElement.children[1]);
-    
+
     headerElement.insertBefore(createHeaderColumn('Spread'), sentimentColumn);
     headerElement.insertBefore(createHeaderColumn('BUY'), sentimentColumn);
     headerElement.insertBefore(createHeaderColumn('SELL'), sentimentColumn);
@@ -156,10 +156,10 @@ function prepareView(contentElement) {
 function createHeaderColumn(text) {
     let columnDiv = document.createElement('div');
     columnDiv.className += 'table-cell small-cell';
-    
+
     let textNode = document.createTextNode(text);
     textNode.className += 'head-label';
-    
+
     columnDiv.appendChild(textNode);
     return columnDiv;
 }
@@ -171,14 +171,14 @@ function start() {
 
 function getOptions() {
     return {
-    refreshTime: 5000
+        refreshTime: 5000
     };
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-                        let doc = event.target;
-                        let win = doc.defaultView;
-                        if (win.frameElement) return;
-                        console.log('starting');
-                        start();
-                        });
+    let doc = event.target;
+    let win = doc.defaultView;
+    if (win.frameElement) return;
+    console.log('starting');
+    start();
+});
